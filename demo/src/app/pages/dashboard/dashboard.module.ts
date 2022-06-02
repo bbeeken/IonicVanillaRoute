050e08976db0affd8ae924b7f1aa990a048b104e
Brian Beeken <Brian.Beeken@LCBHS.net>
2022-06-01 20:41:16 -0600

    ok

--- demo/src/app/pages/dashboard/dashboard.module.ts
+++ demo/src/app/pages/dashboard/dashboard.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { DashboardPageRoutingModule } from './dashboard-routing.module';
-
+import { HttpClientModule } from '@angular/common/http';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { DashboardPage } from './dashboard.page';
 
 @NgModule({
@@ -13,8 +14,13 @@ import { DashboardPage } from './dashboard.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    DashboardPageRoutingModule
+    DashboardPageRoutingModule,
+    HttpClientModule,
+    NgxDatatableModule
   ],
-  declarations: [DashboardPage]
+  declarations: [DashboardPage],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class DashboardPageModule {}
--- demo/src/app/pages/dashboard/dashboard.page.ts
+++ demo/src/app/pages/dashboard/dashboard.page.ts
@@ -1,20 +1,39 @@
+/* eslint-disable @typescript-eslint/semi */
+/* eslint-disable @typescript-eslint/naming-convention */
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { IonicAuthService } from '././../../services/ionic-auth.service';
-
+import { HttpClient } from '@angular/common/http';
+import { ViewEncapsulation } from '@angular/core';
+export interface USERS {
+  users: string;
+}
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 
 export class DashboardPage implements OnInit {
   userDetail: string;
-
+  public Users: USERS;
+  public col: any;
+  public rows: any;
   constructor(
     private router: Router,
-    private ionicAuthService: IonicAuthService
-  ) { }
+    private ionicAuthService: IonicAuthService,
+    private httpClient: HttpClient
+  ) {this.col = [
+    { name: 'Name' },
+    { name: 'Username' },
+    { name: 'email' }
+  ];   this.httpClient.get<USERS>('http://192.168.0.144:3000/users')
+  .subscribe((response) => {
+
+    this.rows = response;
+     console.log(this.col,this.rows)
+  });}
 
   ngOnInit() {
     this.ionicAuthService.userDetails().subscribe(response => {
